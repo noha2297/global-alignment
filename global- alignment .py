83c0427c94911ebdaf046ca9a7d233ebcc669ecf
noha2297 <nohagamalmekky@gmail.com>
2018-06-08 16:06:00 +0200

    first commit

---
+++ global- alignment .py
@@ -0,0 +1,287 @@
+BLOSUM80 = {
+           'A':{'A':5,'R':-2,'N':-2,'D':-2,'C':-1,'Q':-1,'E':-1,'G':0,'H':-2,'I':-2,'L':-2,'K':-1,'M':-1,'F':-3,'P':-1,'S':1,'T':0,'W':-3,'Y':-2,'V':0,'B':-2,'J':-2,'Z':-1,'X':-1,'*':-6},
+           'R':{'A':-2,'R':6,'N':-1,'D':-2,'C':-4,'Q':1,'E':-1,'G':-3,'H':0,'I':-3,'L':-3,'K':2,'M':-2,'F':-4,'P':-2,'S':-1,'T':-1,'W':-4,'Y':-3,'V':-3,'B':-1,'J':-3,'Z':0,'X':-1,'*':-6},
+           'N':{'A':-2,'R':-1,'N':6,'D':1,'C':-3,'Q':0,'E':-1,'G':-1,'H':0,'I':-4,'L':-4,'K':0,'M':-3,'F':-4,'P':-3,'S':0,'T':0,'W':-4,'Y':-3,'V':-4,'B':5,'J':-4,'Z':0,'X':-1,'*':-6},
+           'D':{'A':-2,'R':-2,'N':1,'D':6,'C':-4,'Q':-1,'E':1,'G':-2,'H':-2,'I':-4,'L':-5,'K':-1,'M':-4,'F':-4,'P':-2,'S':-1,'T':-1,'W':-6,'Y':-4,'V':-4,'B':5,'J':-5,'Z':1,'X':-1,'*':-6},
+           'C':{'A':-1,'R':-4,'N':-3,'D':-4,'C':9,'Q':-4,'E':-5,'G':-4,'H':-4,'I':-2,'L':-2,'K':-4,'M':-2,'F':-3,'P':-4,'S':-2,'T':-1,'W':-3,'Y':-3,'V':-1,'B':-4,'J':-2,'Z':-4,'X':-1,'*':-6},
+           'Q':{'A':-1,'R':1,'N':0,'D':-1,'C':-4,'Q':6,'E':2,'G':-2,'H':1,'I':-3,'L':-3,'K':1,'M':0,'F':-4,'P':-2,'S':0,'T':-1,'W':-3,'Y':-2,'V':-3,'B':0,'J':-3,'Z':4,'X':-1,'*':-6},
+           'E':{'A':-1,'R':-1,'N':-1,'D':1,'C':-5,'Q':2,'E':6,'G':-3,'H':0,'I':-4,'L':-4,'K':1,'M':-2,'F':-4,'P':-2,'S':0,'T':-1,'W':-4,'Y':-3,'V':-3,'B':1,'J':-4,'Z':5,'X':-1,'*':-6},
+           'G':{'A':0,'R':-3,'N':-1,'D':-2,'C':-4,'Q':-2,'E':-3,'G':6,'H':-3,'I':-5,'L':-4,'K':-2,'M':-4,'F':-4,'P':-3,'S':-1,'T':-2,'W':-4,'Y':-4,'V':-4,'B':-1,'J':-5,'Z':-3,'X':-1,'*':-6},
+           'H':{'A':-2,'R':0,'N':0,'D':-2,'C':-4,'Q':1,'E':0,'G':-3,'H':8,'I':-4,'L':-3,'K':-1,'M':-2,'F':-2,'P':-3,'S':-1,'T':-2,'W':-3,'Y':2,'V':-4,'B':-1,'J':-4,'Z':0,'X':-1,'*':-6},
+           'I':{'A':-2,'R':-3,'N':-4,'D':-4,'C':-2,'Q':-3,'E':-4,'G':-5,'H':-4,'I':5,'L':1,'K':-3,'M':1,'F':-1,'P':-4,'S':-3,'T':-1,'W':-3,'Y':-2,'V':3,'B':-4,'J':3,'Z':-4,'X':-1,'*':-6},
+           'L':{'A':-2,'R':-3,'N':-4,'D':-5,'C':-2,'Q':-3,'E':-4,'G':-4,'H':-3,'I':1,'L':4,'K':-3,'M':2,'F':0,'P':-3,'S':-3,'T':-2,'W':-2,'Y':-2,'V':1,'B':-4,'J':3,'Z':-3,'X':-1,'*':-6},
+           'K':{'A':-1,'R':2,'N':0,'D':-1,'C':-4,'Q':1,'E':1,'G':-2,'H':-1,'I':-3,'L':-3,'K':5,'M':-2,'F':-4,'P':-1,'S':-1,'T':-1,'W':-4,'Y':-3,'V':-3,'B':-1,'J':-3,'Z':1,'X':-1,'*':-6},
+           'M':{'A':-1,'R':-2,'N':-3,'D':-4,'C':-2,'Q':0,'E':-2,'G':-4,'H':-2,'I':1,'L':2,'K':-2,'M':6,'F':0,'P':-3,'S':-2,'T':-1,'W':-2,'Y':-2,'V':1,'B':-3,'J':2,'Z':-1,'X':-1,'*':-6},
+           'F':{'A':-3,'R':-4,'N':-4,'D':-4,'C':-3,'Q':-4,'E':-4,'G':-4,'H':-2,'I':-1,'L':0,'K':-4,'M':0,'F':6,'P':-4,'S':-3,'T':-2,'W':0,'Y':3,'V':-1,'B':-4,'J':0,'Z':-4,'X':-1,'*':-6},
+           'P':{'A':-1,'R':-2,'N':-3,'D':-2,'C':-4,'Q':-2,'E':-2,'G':-3,'H':-3,'I':-4,'L':-3,'K':-1,'M':-3,'F':-4,'P':8,'S':-1,'T':-2,'W':-5,'Y':-4,'V':-3,'B':-2,'J':-4,'Z':-2,'X':-1,'*':-6},
+           'S':{'A':1,'R':-1,'N':0,'D':-1,'C':-2,'Q':0,'E':0,'G':-1,'H':-1,'I':-3,'L':-3,'K':-1,'M':-2,'F':-3,'P':-1,'S':5,'T':1,'W':-4,'Y':-2,'V':-2,'B':0,'J':-3,'Z':0,'X':-1,'*':-6},
+           'T':{'A':0,'R':-1,'N':0,'D':-1,'C':-1,'Q':-1,'E':-1,'G':-2,'H':-2,'I':-1,'L':-2,'K':-1,'M':-1,'F':-2,'P':-2,'S':1,'T':5,'W':-4,'Y':-2,'V':0,'B':-1,'J':-1,'Z':-1,'X':-1,'*':-6},
+           'W':{'A':-3,'R':-4,'N':-4,'D':-6,'C':-3,'Q':-3,'E':-4,'G':-4,'H':-3,'I':-3,'L':-2,'K':-4,'M':-2,'F':0,'P':-5,'S':-4,'T':-4,'W':11,'Y':2,'V':-3,'B':-5,'J':-3,'Z':-3,'X':-1,'*':-6},
+           'Y':{'A':-2,'R':-3,'N':-3,'D':-4,'C':-3,'Q':-2,'E':-3,'G':-4,'H':2,'I':-2,'L':-2,'K':-3,'M':-2,'F':3,'P':-4,'S':-2,'T':-2,'W':2,'Y':7,'V':-2,'B':-3,'J':-2,'Z':-3,'X':-1,'*':-6},
+           'V':{'A':0,'R':-3,'N':-4,'D':-4,'C':-1,'Q':-3,'E':-3,'G':-4,'H':-4,'I':3,'L':1,'K':-3,'M':1,'F':-1,'P':-3,'S':-2,'T':0,'W':-3,'Y':-2,'V':4,'B':-4,'J':2,'Z':-3,'X':-1,'*':-6},
+           'B':{'A':-2,'R':-1,'N':5,'D':5,'C':-4,'Q':0,'E':1,'G':-1,'H':-1,'I':-4,'L':-4,'K':-1,'M':-3,'F':-4,'P':-2,'S':0,'T':-1,'W':-5,'Y':-3,'V':-4,'B':5,'J':-4,'Z':0,'X':-1,'*':-6},
+           'J':{'A':-2,'R':-3,'N':-4,'D':-5,'C':-2,'Q':-3,'E':-4,'G':-5,'H':-4,'I':3,'L':3,'K':-3,'M':2,'F':0,'P':-4,'S':-3,'T':-1,'W':-3,'Y':-2,'V':2,'B':-4,'J':3,'Z':-3,'X':-1,'*':-6},
+           'Z':{'A':-1,'R':0,'N':0,'D':1,'C':-4,'Q':4,'E':5,'G':-3,'H':0,'I':-4,'L':-3,'K':1,'M':-1,'F':-4,'P':-2,'S':0,'T':-1,'W':-3,'Y':-3,'V':-3,'B':0,'J':-3,'Z':5,'X':-1,'*':-6},
+           'X':{'A':-1,'R':-1,'N':-1,'D':-1,'C':-1,'Q':-1,'E':-1,'G':-1,'H':-1,'I':-1,'L':-1,'K':-1,'M':-1,'F':-1,'P':-1,'S':-1,'T':-1,'W':-1,'Y':-1,'V':-1,'B':-1,'J':-1,'Z':-1,'X':-1,'*':-6},
+           '*':{'A':-6,'R':-6,'N':-6,'D':-6,'C':-6,'Q':-6,'E':-6,'G':-6,'H':-6,'I':-6,'L':-6,'K':-6,'M':-6,'F':-6,'P':-6,'S':-6,'T':-6,'W':-6,'Y':-6,'V':-6,'B':-6,'J':-6,'Z':-6,'X':-6,'*':1},
+}
+def initilize_matrix(seq1,seq2,h,g , sample):
+    
+    infinity=-1000000
+    max_score =infinity
+    max_score_m =infinity
+    max_score_ix =infinity
+    max_score_iy =infinity
+    
+    c=len(seq1)#x "j"
+    r=len(seq2)#y "i"
+    smple=''
+
+    # first , we must initialize the matrix
+    m = [[0 for j in range(c+1)] for i in range(r+1)] 
+    ix = [[0 for j in range(c+1)] for i in range(r+1)] 
+    iy = [[0 for j in range(c+1)] for i in range(r+1)] 
+        
+   # m[0][0]=0
+    iy[0][0]=h+g*0 # initialization of the first cell
+    ix[0][0]=h+g*0
+
+    # initialization of the first row and column
+    for i in range (1 ,r+1):
+        m[i][0]=infinity
+    for j in range (1,c+1):
+        m[0][j]=infinity
+        
+    for i in range (1,r+1):
+        ix[i][0]=h+g*i
+        iy[i][0]=infinity  
+        
+    for j in range (1,c+1):
+        ix[0][j]=infinity
+        iy[0][j]=h+g*j
+        
+    
+                
+    # fill the matrix
+    for i in range (1,r+1):
+        for j in range (1,c+1):
+            if(sample == 'DNA'):
+                smple ='DNA'
+                if seq1[j-1]==seq2[i-1]:
+                    score=2 # match
+                else :
+                    score = -1 # mismatch
+                    
+                m[i][j]=max(m[i-1][j-1]+score,ix[i-1][j-1]+score,iy[i-1][j-1]+score)
+                ix[i][j]=max(m[i-1][j]+h+g,ix[i-1][j]+g)
+                iy[i][j]=max(m[i][j-1]+h+g,iy[i][j-1]+g)
+                
+            
+            elif(sample == 'Protein'):
+                smple = 'Protein'
+                #score= BLOSUM80[seq1[j-1]][seq2[i-1]]
+                score= BLOSUM80.get(seq1[j-1]).get(seq2[i-1])
+                
+                m[i][j]=max(m[i-1][j-1]+score,ix[i-1][j-1]+score,iy[i-1][j-1]+score)
+                ix[i][j]=max(m[i-1][j]+h+g,ix[i-1][j]+g)
+                iy[i][j]=max(m[i][j-1]+h+g,iy[i][j-1]+g)
+
+                
+    final_max = max( m[r][c] ,ix[r][c] , iy[r][c])#Find the max SCore of the last cell in the matrix 
+
+    sequance1=''
+    sequance2=''
+    GA_Match =''
+    choice =''
+    
+    c=len(seq1)#x "j"
+    r=len(seq2)#y "i"
+    
+    # this to find the matrix that contain the max score
+    if (m[r][c] > ix[r][c] and m[r][c] > iy[r][c]):
+        choice ='m'
+        trace_back (seq1, seq2, m ,ix , iy, r, c ,choice ,sequance1 , sequance2 ,GA_Match , g , h , smple)
+
+    elif (ix[r][c] > m[r][c] and ix[r][c] > iy[r][c]):
+        choice = 'x'
+        trace_back (seq1, seq2, m ,ix , iy, r, c ,choice ,sequance1 , sequance2 ,GA_Match , g , h , smple)
+
+    elif (iy[r][c] > m[r][c] and iy[r][c] > ix[r][c]):
+        choice = 'y'
+        trace_back (seq1, seq2, m ,ix , iy, r, c ,choice ,sequance1 , sequance2 ,GA_Match , g , h , smple)
+
+    
+    elif (m[r][c] >= ix[r][c] and m[r][c] >= iy[r][c]):
+        if(m[r][c] == ix[r][c] ):
+            choice ='m'
+            trace_back (seq1, seq2, m ,ix , iy, r, c ,choice ,sequance1 , sequance2 ,GA_Match , g , h , smple)
+            choice ='x'
+            trace_back (seq1, seq2, m ,ix , iy, r, c ,choice ,sequance1 , sequance2 ,GA_Match , g , h , smple)
+            
+        elif(m[r][c] == iy[r][c]):
+            choice ='m'
+            trace_back (seq1, seq2, m ,ix , iy, r, c ,choice ,sequance1 , sequance2 ,GA_Match , g , h , smple)
+            choice ='y'
+            trace_back (seq1, seq2, m ,ix , iy, r, c ,choice ,sequance1 , sequance2 ,GA_Match , g , h , smple)
+                
+    elif (ix[r][c] >= m[r][c] and ix[r][c] >= iy[r][c]):
+        if(ix[r][c] == m[r][c] ):
+            choice ='x'
+            trace_back (seq1, seq2, m ,ix , iy, r, c ,choice ,sequance1 , sequance2 ,GA_Match , g , h , smple)
+            choice ='m'
+            trace_back (seq1, seq2, m ,ix , iy, r, c ,choice ,sequance1 , sequance2 ,GA_Match , g , h , smple)
+                
+        elif(ix[r][c] == iy[r][c]):
+            choice ='x'
+            trace_back (seq1, seq2, m ,ix , iy, r, c ,choice ,sequance1 , sequance2 ,GA_Match , g , h , smple)
+            choice ='y'
+            trace_back (seq1, seq2, m ,ix , iy, r, c ,choice ,sequance1 , sequance2 ,GA_Match , g , h , smple)
+                
+    elif (iy[r][c] >= m[r][c] and iy[r][c] >= ix[r][c]):
+        if(iy[r][c] == m[r][c] ):
+            choice ='y'
+            trace_back (seq1, seq2, m ,ix , iy, r, c ,choice ,sequance1 , sequance2 ,GA_Match , g , h , smple)
+            choice ='m'
+            trace_back (seq1, seq2, m ,ix , iy, r, c ,choice ,sequance1 , sequance2 ,GA_Match , g , h , smple)
+                
+        elif(iy[r][c] == ix[r][c]):
+            choice ='x'
+            trace_back (seq1, seq2, m ,ix , iy, r, c ,choice ,sequance1 , sequance2 ,GA_Match , g , h , smple)
+            choice ='y'
+            trace_back (seq1, seq2, m ,ix , iy, r, c ,choice ,sequance1 , sequance2 ,GA_Match , g , h , smple)
+
+            
+                
+
+    #for i in range (r+1):
+        #for j in range (c+1):
+            #print(m[i][j] ,ix[i][j] , iy[i][j])
+        
+    print ("maximun score of alignment is :" ,final_max)
+
+
+            
+    
+    
+   
+
+def trace_back (seq1, seq2, m ,ix , iy, r, c ,choice ,sequance1 , sequance2 ,GA_Match, g , h ,smple):
+
+    if (r ==0 and c==0):
+        f=open("task1.txt" , "w+")
+        f.write(sequance1[::-1])
+        f.write('\n')
+        f.write(GA_Match[::-1])
+        f.write('\n')
+        f.write(sequance2[::-1])
+        f.write('\n')
+        f.close()
+        print(sequance1[::-1])
+        print(GA_Match[::-1])
+        print(sequance2[::-1])
+        sequance1 = ''
+        sequance2 = ''
+        GA_Match ='' 
+        return()
+    
+    if (choice == 'm'):
+        if (smple =='DNA'):
+            if (seq1[c-1] == seq2[r-1]):
+                score=2
+            else :
+                score = -1
+            
+        elif(smple == 'Protein'):
+            #score= BLOSUM80[seq1[c-1]][seq2[r-1]]
+            score= BLOSUM80.get(seq1[c-1]).get(seq2[r-1])
+
+
+        GA_Match+='|'
+            
+        #steps : 1) we insert the characters in the string that will be displayed 
+        sequance1 += seq1[c-1]
+        sequance2 += seq2[r-1]
+
+        #2) I am in the matrix m , so the choices , iam from left (m ,ix , iy ) 
+        
+        if (m[r][c] == m[r-1][c-1] +score):
+            choice ='m'
+            trace_back (seq1, seq2, m ,ix , iy, r-1, c-1 ,choice ,sequance1 , sequance2,GA_Match, g, h ,smple)
+            
+        
+            
+        if (m[r][c] == ix[r-1][c-1] +score):
+            choice ='x'
+            trace_back (seq1, seq2, m ,ix , iy, r-1, c-1 ,choice ,sequance1 , sequance2,GA_Match , g, h,smple)
+            
+            
+        if (m[r][c] == iy[r-1][c-1] +score):
+            choice ='y'
+            trace_back (seq1, seq2, m ,ix , iy, r-1, c-1 ,choice ,sequance1 , sequance2,GA_Match, g, h,smple)
+
+
+     
+    elif(choice == 'x'):
+        
+        #steps : 1) we insert the characters in the string that will be displayed 
+        sequance1+= '-'
+        GA_Match +=' '
+        sequance2 += seq2[r-1]
+
+        # 2) I am in the matrix m , so the choices , iam from up (m , ix  )
+        if(ix[r][c] == ix[r-1][c]+g):            
+            choice ='x'
+            trace_back (seq1, seq2, m ,ix , iy, r-1, c ,choice ,sequance1 , sequance2 ,GA_Match, g , h,smple)
+            
+        if(ix[r][c]== m[r-1][c]+g+h):
+            choice ='m'
+            trace_back (seq1, seq2, m ,ix , iy, r-1, c ,choice ,sequance1 , sequance2,GA_Match, g, h,smple)
+            
+    
+    elif (choice =='y'):
+
+        #steps : 1) we insert the characters in the string that will be displayed
+        sequance1 += seq1[c-1]
+        GA_Match += ' '
+        sequance2 += '-'
+
+        #2)I am in the matrix m , so the choices , iam from left (m , iy ) 
+        if(iy[r][c] == iy[r][c-1]+g):
+            choice ='y'
+            trace_back (seq1, seq2, m ,ix , iy, r, c-1 ,choice ,sequance1 , sequance2,GA_Match, g ,h,smple)
+
+        if(iy[r][c]== m[r][c-1]+g+h):
+            choice ='m'
+            trace_back (seq1, seq2, m ,ix , iy, r , c-1 ,choice ,sequance1 , sequance2,GA_Match, g, h,smple)
+
+    
+    
+          
+            
+
+
+def to_take ():
+    print ("Choose 1 for DNA aligment")
+    print ("Choose 2 for Protein aligment")
+    go= int (input ())
+    print ("First Sequance")
+    FirstSeq=input()
+    print ("Second Sequance")
+    SecondSeq= input()
+    print ("Gap Opening ")
+    OpenGap=int(input()) #h
+    print ("Gap Extention ")
+    ExtentionGap=int(input()) #g
+    if go == 1:
+        sample= 'DNA'
+        initilize_matrix(FirstSeq,SecondSeq,OpenGap,ExtentionGap ,sample)
+        #trace_back (seq1, seq2, m ,ix , iy, r, c ,choice ,sequance1 , sequance2 , g , h)
+    else :
+        sample= 'Protein'
+        initilize_matrix(FirstSeq,SecondSeq,OpenGap,ExtentionGap ,sample)
+             
+            
+            
+        
+    
+to_take() 
